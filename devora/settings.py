64a078b0bf163efc936237d0c307b9429f416b0a
RjazanovEgor <rjazanovegor@mail.ru>
2021-10-10 04:45:14 +0300

    base v2.2.3

--- devora/settings.py
+++ devora/settings.py
@@ -125,11 +125,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media/')
-MEDIA_URL = '/static/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media/')
+# MEDIA_URL = '/static/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
